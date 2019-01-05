534d9f340969db434e2d0d1b3b221ccefc8c7353
coderdream <coderdream@coderdreamdeMacBook-Pro.local>
2019-01-05 12:15:05 +0800

    02_06-Custom scenes and custom classes 20190105

---
+++ Chapter01/Basics/Basics/MainMenu.swift
@@ -0,0 +1,16 @@
+//
+//  MainMenu.swift
+//  Basics
+//
+//  Created by coderdream on 2019/1/5.
+//  Copyright © 2019 coderdream. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+
+    override func didMove(to view: SKView) {
+        print("custom class running!")
+    }
+}
